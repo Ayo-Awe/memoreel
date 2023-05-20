61296d28458edfd9cd7a1a37ba7daa4649f74cd6
Awe Ayomidipupo <pupoawe@gmail.com>
2023-05-20 01:13:59 +0100

    chore: setup custom error classes

---
+++ server/src/errors/httpErrorCodes.ts
@@ -0,0 +1,21 @@
+export type ResourceNotFoundErrorCode =
+  | "RESOURCE_NOT_FOUND"
+  | "UNKNOWN_ENDPOINT";
+
+export type BadRequestErrorCode =
+  | "INVALID_REQUEST_PARAMETERS"
+  | "MISSING_REQUIRED_FIELD"
+  | "INVALID_JSON_FORMAT";
+
+export type UnauthorizedErrorCode =
+  | "EXPIRED_TOKEN"
+  | "MISSING_AUTH_HEADER"
+  | "MALFORMED_TOKEN"
+  | "INVALID_TOKEN";
+
+export type ForbiddenErrorCode = "ACCESS_DENIED" | "INSUFFICIENT_PERMISSIONS";
+
+export type ServerErrorCode =
+  | "UNEXPECTED_ERROR"
+  | "DATABASE_ERROR"
+  | "THIRD_PARTY_API_FAILURE";
---
+++ server/src/errors/httpErrors.ts
@@ -0,0 +1,49 @@
+import {
+  BadRequestErrorCode,
+  ForbiddenErrorCode,
+  ResourceNotFoundErrorCode,
+  ServerErrorCode,
+  UnauthorizedErrorCode,
+} from "./httpErrorCodes";
+
+export abstract class HttpError extends Error {
+  errorCode: string;
+  statusCode: number;
+
+  constructor(statusCode: number, message: string, errorCode: string) {
+    super(message);
+    this.name = this.constructor.name;
+    this.errorCode = errorCode;
+    this.statusCode = statusCode;
+  }
+}
+
+export class BadRequest extends HttpError {
+  constructor(message: string, errorCode: BadRequestErrorCode) {
+    super(400, message, errorCode);
+  }
+}
+
+export class ResourceNotFound extends HttpError {
+  constructor(message: string, errorCode: ResourceNotFoundErrorCode) {
+    super(404, message, errorCode);
+  }
+}
+
+export class Unauthorized extends HttpError {
+  constructor(message: string, errorCode: UnauthorizedErrorCode) {
+    super(401, message, errorCode);
+  }
+}
+
+export class Forbidden extends HttpError {
+  constructor(message: string, errorCode: ForbiddenErrorCode) {
+    super(403, message, errorCode);
+  }
+}
+
+export class ServerError extends HttpError {
+  constructor(message: string, errorCode: ServerErrorCode) {
+    super(500, message, errorCode);
+  }
+}
