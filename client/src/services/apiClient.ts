e0f595db9e34b2b85dfc041ebdcdfd348dcd6343
Awe Ayomidipupo <pupoawe@gmail.com>
2023-06-01 15:14:35 +0100

    feat: update api client

--- client/src/services/apiClient.ts
+++ client/src/services/apiClient.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 console.log(process.env.NEXT_PUBLIC_API_BASE_URL);
 export default axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL + "/api/v1",
 });
